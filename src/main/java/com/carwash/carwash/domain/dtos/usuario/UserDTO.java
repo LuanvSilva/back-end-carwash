5078c1f951549ee39e2209dec5da201ba349feb1
LuanvSilva <luanlvs2010@gmail.com>
2024-08-29 21:43:53 -0300

    feat: update de usuario dto

--- src/main/java/com/carwash/carwash/domain/dtos/usuario/UserDTO.java
+++ src/main/java/com/carwash/carwash/domain/dtos/usuario/UserDTO.java
@@ -1,5 +1,12 @@
 package com.carwash.carwash.domain.dtos.usuario;
+import lombok.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Data
 public class UserDTO {
 
     private Long id;
@@ -8,45 +15,5 @@ public class UserDTO {
     private Long empresa_moon;
     private boolean ativo;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getEmpresa_moon() {
-        return empresa_moon;
-    }
-
-    public void setEmpresa_moon(Long empresa_moon) {
-        this.empresa_moon = empresa_moon;
-    }
-
-
-    public boolean isAtivo() {
-        return ativo;
-    }
-
-    public void setAtivo(boolean ativo) {
-        this.ativo = ativo;
-    }
+   
 }
